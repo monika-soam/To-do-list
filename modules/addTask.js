56a4b2f949ec1c41992f63fa6719fc4e757e92fd
Monika-Soam17 <86161447+Monika-Soam17@users.noreply.github.com>
2022-03-23 00:06:21 +0530

    fixed linters

--- modules/addTask.js
+++ modules/addTask.js
@@ -1,7 +1,6 @@
 import { getTaskFromLocalStorage, setTaskIntoLocalStorage } from './localStorage.js';
 
 const addTask = (text) => {
-
   let tasks = getTaskFromLocalStorage();
   tasks = tasks.sort((v1, v2) => v1.index - v2.index);
   const index = tasks[tasks.length - 1].index + 1;
